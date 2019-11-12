1ea80023445173a8ef321b5d5fac0c55af5c11df
obada almatrami <obada.matrami92@gmail.com>
2019-11-12 23:10:51 +0200

    final-edit lab2

--- js/app.js
+++ js/app.js
@@ -1,65 +1,61 @@
 'use strict';
 
-function RandomHorn(animal) {
-    this.keyword = animal.keyword;
-    this.image_url = animal.image_url;
-    this.title = animal.title;
-    this.description = animal.description;
-    this.horns = animal.horns;
+function Horns(data) {
+    this.image_url = data.image_url;
+    this.title = data.title;
+    this.description = data.description;
+    this.keyword = data.keyword;
+    this.horns = data.horns;
+    Horns.all.push(this);
 }
-RandomHorn.allCreatures = [];
+Horns.all = [];
 
-RandomHorn.prototype.render = function() {
-    $('main').append('<div class="clone"></div>');
-    let Hornoutput = $('div[class="clone"]');
-    let creatureHtml = $('#photo-template').html();
-    Hornoutput.html(creatureHtml);
-    Hornoutput.find('h2').text(this.title);
-    Hornoutput
-        .find('img')
-        .attr('src', this.image_url)
-        .attr('alt', this.description);
-    Hornoutput.find('p').text(this.description);
-    Hornoutput.removeClass('clone');
-    Hornoutput.attr('class', this.keyword).addClass('animal');
+Horns.prototype.render = function() {
+
+    // Create a new empty div tag
+    let hornOutput = $('<div></div>');
+    hornOutput.addClass(this.keyword);
+
+    // clone (copy) the html from inside the photo-template
+    let template = $('#photo-template').html();
+
+    // Add the template to the output div
+    hornOutput.html(template);
+
+    // Put the data in
+    hornOutput.find('h2').text(this.title);
+    hornOutput.find('img').attr('src', this.image_url);
+    hornOutput.find('p').text(this.description);
+
+    $('main').append(hornOutput);
+
 };
 
-RandomHorn.readJson = () => {
-    $.get('../data/page-1.json', 'json')
-        .then(data => {
-            data.forEach(item => {
-                RandomHorn.allCreatures.push(new RandomHorn(item));
-            });
-        })
-        .then(RandomHorn.loadCreatures);
-};
-
-RandomHorn.loadCreatures = () => {
-    RandomHorn.allCreatures.forEach(item => item.render());
-    RandomHorn.makeOption();
-};
-
-$(() => RandomHorn.readJson());
-RandomHorn.SelectOptions = [];
-
-RandomHorn.makeOption = function() {
-    let SelectOptionsClone = this.SelectOptions;
-    RandomHorn.allCreatures.forEach(function(critter) {
-        if ($.inArray(critter.keyword, SelectOptionsClone) === -1) {
-            SelectOptionsClone.push(critter.keyword);
+function populateSelectBox() {
+    let seen = {};
+    let select = $('select');
+    Horns.all.forEach((horn) => {
+        if (!seen[horn.keyword]) {
+            let option = `<option value="${horn.keyword}">${horn.keyword}</option>`;
+            select.append(option);
+            seen[horn.keyword] = true;
         }
     });
-    for (let i = 0; i < SelectOptionsClone.length; i++) {
-        $('select').append(
-            '<option value=' + SelectOptionsClone[i] + '>' + SelectOptionsClone[i] + '</option>'
-        );
-    }
-};
 
+    console.log(seen);
+}
 
-$('select[name="choice"]').on('change', function() {
-    let $selection = $(this).val();
-    $('.animal').hide();
-    $('.' + $selection).show();
+$('select').on('change', function() {
+    let selected = $(this).val();
+    $('div').hide();
+    $(`.${selected}`).fadeIn(800);
+});
 
-})
+$.get('../data/page-1.json')
+    .then(data => {
+        data.forEach((thing) => {
+            let horn = new Horns(thing);
+            horn.render();
+        });
+    })
+    .then(() => populateSelectBox());
